032ca449d6cc90e78b5117f501e7d6f2d56bcd26
Project-j7 <projectj724@gmail.com>
2024-08-30 13:35:49 +0530

    Add all edited and new files

--- signup.js
+++ signup.js
@@ -15,7 +15,7 @@ document.getElementById('signupForm').addEventListener('submit', async (e) => {
     }
 
     try {
-        const response = await fetch('https://jewellerys-projects-j7.vercel.app/routes/auth/signup', {
+        const response = await fetch('https://jewellerys-projects-j7.vercel.app/api/auth/signup', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
